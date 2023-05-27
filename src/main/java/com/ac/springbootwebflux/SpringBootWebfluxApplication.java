4c168bf7aef384a1c9610ce3422a3a04845816ee
Anthony Carrasco <thony10.rc.c@gmail.com>
2023-05-27 18:32:27 -0500

    remove rows of the documents collection

--- src/main/java/com/ac/springbootwebflux/SpringBootWebfluxApplication.java
+++ src/main/java/com/ac/springbootwebflux/SpringBootWebfluxApplication.java
@@ -7,17 +7,20 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class SpringBootWebfluxApplication implements CommandLineRunner {
 
     private final ProductDao dao;
+    private final ReactiveMongoTemplate reactiveMongoTemplate;
 
     private static  final Logger log = LoggerFactory.getLogger(SpringBootWebfluxApplication.class);
 
-    public SpringBootWebfluxApplication(ProductDao dao) {
+    public SpringBootWebfluxApplication(ProductDao dao, ReactiveMongoTemplate reactiveMongoTemplate) {
         this.dao = dao;
+        this.reactiveMongoTemplate = reactiveMongoTemplate;
     }
 
     public static void main(String[] args) {
@@ -26,6 +29,9 @@ public class SpringBootWebfluxApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        // ejemplo de como eliminar data solo usar en ejemplos no en PROD
+        reactiveMongoTemplate.dropCollection("products").subscribe();
+
         Flux.just(new Product("Laptop1", 100.10),
                         new Product("Laptop2", 200.10),
                         new Product("Laptop3", 300.10),
