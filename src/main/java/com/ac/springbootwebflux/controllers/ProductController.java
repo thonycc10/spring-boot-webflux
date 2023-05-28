ffcbb2306910753e8c47bd19ae2038f5bf585afd
Anthony Carrasco <thony10.rc.c@gmail.com>
2023-05-28 16:39:45 -0500

    finish view image

--- src/main/java/com/ac/springbootwebflux/controllers/ProductController.java
+++ src/main/java/com/ac/springbootwebflux/controllers/ProductController.java
@@ -8,7 +8,10 @@ import jakarta.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.data.mongodb.core.aggregation.ArithmeticOperators;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,6 +23,9 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.io.File;
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.Duration;
 import java.util.Date;
 import java.util.UUID;
@@ -63,6 +69,19 @@ public class ProductController {
                 .then(Mono.just("view"))
                 .onErrorResume(ex -> Mono.just("redirect:/list?error=not+exist+product")
                 );
+    }
+
+    @GetMapping("/uploads/img/{namePicture:.+}")
+    public Mono<ResponseEntity<Resource>> viewPicture(@PathVariable String namePicture) throws MalformedURLException {
+        Path ruta = Paths.get(pathImg).resolve(namePicture).toAbsolutePath();
+
+        Resource img = new UrlResource(ruta.toUri());
+
+        return Mono.just(
+                ResponseEntity.ok()
+                        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + img.getFilename() + "\"")
+                        .body(img)
+        );
     }
 
     @GetMapping({"/list", "/"})
