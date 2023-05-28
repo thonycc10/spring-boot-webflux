a470741e7d56a3175e191cb4e3a74c143bb7aad9
Anthony Carrasco <thony10.rc.c@gmail.com>
2023-05-28 15:02:18 -0500

    add category, add select html, create method addatribute list

--- src/main/java/com/ac/springbootwebflux/controllers/ProductController.java
+++ src/main/java/com/ac/springbootwebflux/controllers/ProductController.java
@@ -1,6 +1,8 @@
 package com.ac.springbootwebflux.controllers;
 
+import com.ac.springbootwebflux.documents.Category;
 import com.ac.springbootwebflux.documents.Product;
+import com.ac.springbootwebflux.services.CategoryService;
 import com.ac.springbootwebflux.services.ProductService;
 import jakarta.validation.Valid;
 import org.slf4j.Logger;
@@ -9,10 +11,7 @@ import org.springframework.data.mongodb.core.aggregation.ArithmeticOperators;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 import org.thymeleaf.spring6.context.webflux.ReactiveDataDriverContextVariable;
 import reactor.core.publisher.Flux;
@@ -26,11 +25,18 @@ import java.util.Date;
 public class ProductController {
 
     private final ProductService service;
+    private final CategoryService categoryService;
 
     private static final Logger log = LoggerFactory.getLogger(ProductController.class);
 
-    public ProductController(ProductService service) {
+    public ProductController(ProductService service, CategoryService categoryService) {
         this.service = service;
+        this.categoryService = categoryService;
+    }
+
+    @ModelAttribute("categories")
+    public Flux<Category> categories() {
+        return categoryService.findAll();
     }
 
     @GetMapping({"/list", "/"})
@@ -48,6 +54,7 @@ public class ProductController {
     @GetMapping("/form")
     public Mono<String> createView(Model model) {
         model.addAttribute("product", new Product());
+        model.addAttribute("category", new Category());
         model.addAttribute("title", "Form Product");
         model.addAttribute("btnSumit", "Crear");
 
