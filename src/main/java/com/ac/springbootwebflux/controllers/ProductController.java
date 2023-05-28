8a18a85085a2e145f56b304e593fa447791c12b0
Anthony Carrasco <thony10.rc.c@gmail.com>
2023-05-27 19:36:26 -0500

    add reactive thymeleaf use repeat 5000 data

--- src/main/java/com/ac/springbootwebflux/controllers/ProductController.java
+++ src/main/java/com/ac/springbootwebflux/controllers/ProductController.java
@@ -54,4 +54,18 @@ public class ProductController {
 
         return "list";
     }
+
+    @GetMapping("/list-full")
+    public String listFull(Model model) {
+        Flux<Product> products = dao.findAll()
+                .map(product -> {
+                    product.setName(product.getName().toUpperCase());
+                    return product;
+                }).repeat(5000);
+
+        model.addAttribute("products", products);
+        model.addAttribute("title", "List products");
+
+        return "list";
+    }
 }
